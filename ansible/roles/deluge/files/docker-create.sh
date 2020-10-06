5e414858f85450e331c38cc6bc33413f8f663c29
Azure Devops <azure@devops.com>
2020-10-06 23:01:02 +0100

    fix deluge

--- ansible/roles/deluge/files/docker-create.sh
+++
@@ -1,9 +0,0 @@
-docker create \
-  --name deluge \
-  --net=host \
-  -e PUID=<UID> -e PGID=<GID> \
-  -e TZ=<timezone> \
-  -e UMASK_SET=<022> \
-  -v /media/:/downloads \
-  -v </path/to/deluge/config>:/config \
-  linuxserver/deluge
--- ansible/roles/plex/files/start-plex-docker.sh
+++ ansible/roles/plex/files/start-plex-docker.sh
@@ -1,10 +1,11 @@
+sudo apt install apt-transport-https ca-certificates curl
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt update
+sudo apt install plexmediaserver
+sudo systemctl status plexmediaserver
 
-docker run \
-  -d \
-  --name plex \
-  --net host \
-  -p 32400:32400 \
-  --restart always \
-  --volume /media/4tb/plex/config:/config \
-  --volume /media/4tb:/data \
-  greensheep/plex-server-docker-rpi:latest
+sudo usermod -aG users plex
+sudo usermod -aG plex pi
+sudo usermod -aG video pi
+sudo usermod -aG pi plex
